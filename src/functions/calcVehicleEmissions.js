53c89da6e09ecdf9e124886e4a546417dcea41a5
Wilke <joelwilke@hotmail.com>
2018-02-01 12:54:22 -0600

    Added Maintenance Select option to equation created g_MAINTENANCE_NOT_DO_FACTOR for hard coded constant

--- src/functions/calcVehicleEmissions.js
+++ src/functions/calcVehicleEmissions.js
@@ -1,24 +1,21 @@
 import calcVehicleExhaust from './calcVehicleExhaust.js';
 import { calcMilesPerYear } from '../utils.js';
 
-export default function calcVehicleEmissions(miles, select, mpg){
+import { g_MAINTENANCE_NOT_DO_FACTOR } from '../Constants.js';
+
+export default function calcVehicleEmissions(miles, select, mpg, maintSelect){
 	//var miles = scrubInputText(mileChecker);               //  check for non number related characters
 	//miles = stripCommas(miles);
 	
 	if (select === "Per Week") {                           // convert miles per week to miles per year first
 		miles = calcMilesPerYear(miles);
 	}
-
-	//var mpg = scrubInputText(mpgChecker);
-	//mpg = stripCommas(mpg);
 	
-	return calcVehicleExhaust(miles,mpg);
-	/*
-	if (maintCurrentSelect == "Do Not Do") {
-		exhaust = exhaust * 1.04;
+	let exhaust = calcVehicleExhaust(miles,mpg);
+	
+	if (maintSelect === "Do Not Do") {
+		exhaust = exhaust * g_MAINTENANCE_NOT_DO_FACTOR;
 	}
-	else{
-		exhaust = 0;
-	}
-*/
+	
+	return exhaust;
 }
