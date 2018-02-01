768100dda61427597862324f5864d6dabef49bb6
Wilke <joelwilke@hotmail.com>
2018-02-01 12:30:52 -0600

    Removed Vehicle from App.js Initial commit of Vehicle.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import calcNaturalGas from './functions/calcNaturalGas.js';
 import calcFuelOil from './functions/calcFuelOil.js';
 import calcElectric from './functions/calcElectric.js';
 import calcPropane from './functions/calcPropane.js';
-import calcVehicleEmissions from './functions/calcVehicleEmissions.js';
+
 
 import Readout from './Readout.js';
 import Question from './Question.js';
 import HomeEmissions from './HomeEmissions.js';
+import Vehicle from './Vehicle.js';
 
 
 class Calculator extends Component {
@@ -196,86 +197,7 @@ class VehicleEmissions extends React.Component {
     }
 }
 
-class Vehicle extends React.Component {
-    constructor() {
-    super();
-    
-    this.state = {
-        vehicleMiles: 0,
-        vehicleMilesSelect: '',
-        vehicleMPG: 0,
-        vehicleEmission: 0
-    };
-  }
-  
-    render(){
-        return(
-            <div className="col-sm-6  emitter">
-                {this._getMilesQuestion()}
-                {this._getMPGQuestion()}
-                {this.state.vehicleEmission} lbs of CO2
-            </div>
-        );
-    }
-    
-    _getMilesQuestion() {
-        return <Question
-            id={this.props.id}
-            name={'vehicleMiles' + this.props.id}
-            question={'On average, miles you drive:'}
-            input={'number'}
-            options={['Per Year', 'Per Week']}
-            onUpdate={this._handleInputValues.bind(this)}
-            output={'none'}/>
-    }
-    
-    _getMPGQuestion() {
-        return <Question
-            id={this.props.id + 1}
-            name={'vehicleMPG' + this.props.id}
-            question={'Average gas mileage:'}
-            input={'number'}
-            options={'none'}
-            onUpdate={this._handleInputValues.bind(this)}
-            output={'none'}/>
-    }
-    
-    _setVehicleEmission(){
-        if (this.state.vehicleMiles !== 0 && this.state.vehicleMilesSelect !== '' && this.state.vehicleMPG !== 0){
-            let vehicleEmission = 0;
-            vehicleEmission = calcVehicleEmissions(this.state.vehicleMiles, this.state.vehicleMilesSelect, this.state.vehicleMPG);
-            
-            this.setState({ vehicleEmission }, function(){
-                this._setUpdate();
-            });
-        }
-    }
-    
-    _setUpdate(){
-        this.props.onUpdate(this.props.id, this.state.vehicleEmission);
-    }
-    
-    _handleInputValues(id, inputVal, selectOpt){
-        const name = id.slice(0, -1);
-        
-        if (name === 'vehicleMiles'){
-            this.setState({
-                vehicleMiles: inputVal,
-                vehicleMilesSelect: selectOpt
-            }, function(){
-                this._setVehicleEmission();
-            });
-        }
-        else if (name === 'vehicleMPG'){
-            this.setState({
-                vehicleMPG: inputVal
-            }, function(){
-                this._setVehicleEmission();
-            });
-        }
-    }
-    
-}
+
 
 export default Calculator;
 
---
+++ src/Vehicle.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import calcVehicleEmissions from './functions/calcVehicleEmissions.js';
+import Question from './Question.js';
+
+
+class Vehicle extends React.Component {
+    constructor() {
+    super();
+    
+    this.state = {
+        vehicleMiles: 0,
+        vehicleMilesSelect: '',
+        vehicleMPG: 0,
+        vehicleEmission: 0
+    };
+  }
+  
+    render(){
+        return(
+            <div className="col-sm-6  emitter">
+                {this._getMilesQuestion()}
+                {this._getMPGQuestion()}
+                {this.state.vehicleEmission} lbs of CO2
+            </div>
+        );
+    }
+    
+    _getMilesQuestion() {
+        return <Question
+            id={this.props.id}
+            name={'vehicleMiles' + this.props.id}
+            question={'On average, miles you drive:'}
+            input={'number'}
+            options={['Per Year', 'Per Week']}
+            onUpdate={this._handleInputValues.bind(this)}
+            output={'none'}/>
+    }
+    
+    _getMPGQuestion() {
+        return <Question
+            id={this.props.id + 1}
+            name={'vehicleMPG' + this.props.id}
+            question={'Average gas mileage:'}
+            input={'number'}
+            options={'none'}
+            onUpdate={this._handleInputValues.bind(this)}
+            output={'none'}/>
+    }
+    
+    _setVehicleEmission(){
+        if (this.state.vehicleMiles !== 0 && this.state.vehicleMilesSelect !== '' && this.state.vehicleMPG !== 0){
+            let vehicleEmission = 0;
+            vehicleEmission = calcVehicleEmissions(this.state.vehicleMiles, this.state.vehicleMilesSelect, this.state.vehicleMPG);
+            
+            this.setState({ vehicleEmission }, function(){
+                this._setUpdate();
+            });
+        }
+    }
+    
+    _setUpdate(){
+        this.props.onUpdate(this.props.id, this.state.vehicleEmission);
+    }
+    
+    _handleInputValues(id, inputVal, selectOpt){
+        const name = id.slice(0, -1);
+        
+        if (name === 'vehicleMiles'){
+            this.setState({
+                vehicleMiles: inputVal,
+                vehicleMilesSelect: selectOpt
+            }, function(){
+                this._setVehicleEmission();
+            });
+        }
+        else if (name === 'vehicleMPG'){
+            this.setState({
+                vehicleMPG: inputVal
+            }, function(){
+                this._setVehicleEmission();
+            });
+        }
+    }
+    
+}
+
+
+export default Vehicle;
